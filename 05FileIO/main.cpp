53a9c2614f1aa25393838bdf5ed9b3af172b700f
s179065 <s179065@SEA-8E412E>
2017-09-18 10:11:25 -0700

    Added stuff to file io

--- 05FileIO/main.cpp
+++ 05FileIO/main.cpp
@@ -8,8 +8,46 @@ using std::fstream;
 
 using std::string;
 
+// My Information
+void main2()
+{
+	string inputBuffer;
+
+	cout << "Where would you like to save this?" << endl;
+	cin >> inputBuffer;
+
+	fstream profileData;
+	profileData.open(inputBuffer.c_str(), ios_base::out);
+
+	if (profileData.fail())
+	{
+		cout << "Sorry, it can't be saved there." << endl;
+		return;
+	}
+
+	cin.ignore(1000, '\n');
+	cout << "Who are you?" << endl;
+	getline(cin, inputBuffer);
+	profileData << inputBuffer << endl;
+
+	cin.ignore(1000, '\n');
+	cout << "How old are you?" << endl;
+	getline(cin, inputBuffer);
+	profileData << inputBuffer << endl;
+
+	cin.ignore(1000, '\n');
+	cout << "What's your favorite color?" << endl;
+	getline(cin, inputBuffer);
+	profileData << inputBuffer << endl;
+
+	profileData.flush();
+	profileData.close();
+
+	cout << "Thanks!" << endl;
+}
+
 // WALKTHROUGH
-int main()
+/*int main()
 {
 	fstream file;
 
@@ -56,42 +94,88 @@ int main()
 
 	while (true) {}
 
-}
+}*/
 
 // CLOSED
 // Digital Printer
 int main()
 {
-	cout << "Hello! What file would you like to read?" << endl;
-	int(fileInput);
-	cin >> fileInput;
-	if (fileInput == famousLines.txt)
+	cout << "DIGITAL PRINTER" << endl;
+	while (true)
 	{
+		cout << "What file would you like to open?" << endl;
+		string inputBuffer;
 
+		cin >> inputBuffer;
+		fstream printer;
+
+		printer.open(inputBuffer);
+		if (printer.fail())
+		{
+			cout << "The file you have requested cannot be found." << endl;
+			continue;
+		}
+
+		string fileContents;
+		while (getline(printer, fileContents))
+		{
+			cout << fileContents;
+		}
+
+		file.close;
 	}
-
-	fstream file;
-
-	file.open("famousLines.txt");
-
-	if (file.fail())
-	{
-		cout << "That file does not exist, please choose a new file path to open." << endl;
-		return -1;
-	}
-
-	string buffer;
-	while (getline(file, buffer)) 
-	{
-		cout << buffer << endl;
-	}
-
-	file.clear(); 
-	file.close();
 
 	while (true) {}
 
+	//2
+	main2();
 
-	// Load Entity Data
-	
+	fstream entityStream;
+	entityStream.open("monsters.txt");
+
+	if (entityStream.fail())
+	{
+		cout << "Can't open file" << endl;
+	}
+
+	int entityCount = 0;
+	Entity entities[100];
+
+	while (true)
+	{
+		string buf;
+		bool entityFound = false;
+
+		// seek to the next entity
+		while (getline(entityStream, buf))
+		{
+			if (buf[0] == '@')
+			{
+				entityFound = true;
+				break;
+			}
+		}
+
+		// exit if no entity found
+		if (!entityFound) { break; }
+		
+
+		// Load data into the array of entities
+		Entity& curEntity = entities[entityCount];
+
+		getline(entityStream, buf);
+		curEntity.hitpoints = stof(buf);
+		getline(entityStream, buf);
+		curEntity.armor = stof(buf);
+		getline(entityStream, buf);
+		curEntity.strength = stof(buf);
+		getline(entityStream, buf);
+		curEntity.defense = stof(buf);
+		getline(entityStream, buf);
+		curEntity.agility = stof(buf);
+		getline(entityStream, buf);
+		curEntity.luck = stof(buf);
+	}
+
+	while (true) {}
 }
